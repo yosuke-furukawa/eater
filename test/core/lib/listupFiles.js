99b12f7be14f36ef26d39faa06b816acada2fb31
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-04-08 16:21:53 +0200

    listupFiles: fix resolve path to build file paths
    No need to care about the path.seq
    ```
    const lists = listupFiles('./test/fixture', '.js');
    const lists = listupFiles('./test/fixture/', '.js');
    ```

--- test/core/lib/listupFiles.js
+++ test/core/lib/listupFiles.js
@@ -3,14 +3,14 @@ const listupFiles = require(`${process.cwd()}/lib/listupFiles`);
 
 const lists = listupFiles('./test/fixture', '.js');
 assert(lists.length === 6);
-assert(lists.indexOf('./test/fixture/test/a.js') !== -1);
-assert(lists.indexOf('./test/fixture/test/aaa/bbb/ccc/b.js') !== -1);
-assert(lists.indexOf('./test/fixture/test/aaa/bbb/c.js') !== -1);
-assert(lists.indexOf('./test/fixture/test/aaa/bbb/c.test.js') !== -1);
+assert(lists.indexOf('test/fixture/test/a.js') !== -1);
+assert(lists.indexOf('test/fixture/test/aaa/bbb/ccc/b.js') !== -1);
+assert(lists.indexOf('test/fixture/test/aaa/bbb/c.js') !== -1);
+assert(lists.indexOf('test/fixture/test/aaa/bbb/c.test.js') !== -1);
 
-const listTestJsFiles = listupFiles('./test/fixture', '.test.js');
+const listTestJsFiles = listupFiles('test/fixture', '.test.js');
 assert(listTestJsFiles.length === 1);
-assert(listTestJsFiles.indexOf('./test/fixture/test/aaa/bbb/c.test.js') !== -1);
+assert(listTestJsFiles.indexOf('test/fixture/test/aaa/bbb/c.test.js') !== -1);
 
 assert.throws(() => {
   listupFiles('./test/fixture');
@@ -19,4 +19,3 @@ assert.throws(() => {
 assert.throws(() => {
   listupFiles(null, '.js');
 }, /dir should be string/ );
-
