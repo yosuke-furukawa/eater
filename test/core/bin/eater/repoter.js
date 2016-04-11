b0ce252dc700be88cd193624234b90edc65ead03
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2016-04-12 02:08:46 +0900

    Improve coverage

--- test/core/bin/eater/repoter.js
+++ test/core/bin/eater/repoter.js
@@ -3,7 +3,7 @@ const assert = require('power-assert');
 const Reporter = require(`${process.cwd()}`).Reporter;
 
 const result = cp.execSync(`${process.cwd()}/bin/eater.js --reporter ${process.cwd()}/test/reporter/DotReporter  --dir test/fixture/ --ext success.js`).toString();
-
-
 assert(result.trim() === '.');
 
+const errResult = cp.spawnSync(`${process.cwd()}/bin/eater.js`, ["--reporter", "NotFoundReporter", "--dir", "test/fixture/", "--ext", "js"]);
+assert(errResult.stderr.toString().trim() === "Error: Cannot find module 'NotFoundReporter'");
