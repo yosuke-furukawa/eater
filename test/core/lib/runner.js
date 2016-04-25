5d32f4277e1cc07696fd40cac960c444dda36871
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2016-04-25 18:27:15 +0900

    Add test runner

---
+++ test/core/lib/runner.js
@@ -0,0 +1,42 @@
+const test = require(`${process.cwd()}/lib/runner`).test;
+const assert = require('power-assert');
+const mustCall = require('must-call');
+const originalProcessSend = process.send;
+const originalConsoleError = console.error;
+console.error = () => {};
+
+process.send = mustCall((obj) => {
+  assert(obj.testName === 'useCase truthy');
+});
+
+test('useCase truthy', (resolve, reject) => {
+  assert(true);
+  resolve();
+});
+
+process.send = mustCall((obj) => {
+  assert(obj.testName === 'useCase falsy');
+});
+
+test('useCase falsy', (resolve, reject) => {
+  assert(1 === 2);
+  resolve();
+});
+
+process.send = mustCall((obj) => {
+  assert(obj.testName === 'throw error');
+});
+
+test('throw error', (resolve, reject) => {
+  throw new Error('e');
+});
+
+process.send = mustCall((obj) => {
+  assert(obj.testName === 'must call');
+});
+
+test('must call', (resolve, reject) => {
+  Promise.resolve(100).then(mustCall((value) => {
+    assert(value !== 100);
+  })) 
+});
