c513567c5d299d81c0e2ff152195eaf082c03aca
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2016-11-06 10:53:36 +0900

    test: improve coverage

--- test/only/only.js
+++ test/only/only.js
@@ -4,11 +4,10 @@ const test = require(`${process.cwd()}/lib/runner`).test;
 const mustCall = require('must-call');
 const assert = require('power-assert');
 
-test('this test should not execute', (_, fail) =>{
-  fail('should not be executed');
-});
-
 only('only is executed', () => {
   assert(true);
 });
 
+test('this test should not execute', (_, fail) =>{
+  fail('should not be executed');
+});
